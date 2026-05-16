7514129b2356adea242719c41a90ff04d8bc1626
Joko Prasetyo <97484146+jokoprasetyo-id@users.noreply.github.com>
2026-05-16 17:27:39 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,51 @@
-// File sw.js kosong ini sudah cukup untuk memicu tombol "Install PWA" di browser.
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'gpm-cache-v1';
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache berhasil dibuka');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Pengecualian untuk request ke API eksternal (seperti Firebase Firebase)
+  // Biarkan request Firebase selalu berjalan melalui jaringan internet
+  if (event.request.url.includes('firebaseio.com') || event.request.url.includes('googleapis.com')) {
+      return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - kembalikan response dari cache (bikin aplikasi loading super cepat)
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
